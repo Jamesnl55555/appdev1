9eb5752ed2ce1eed09b485cffd612fa078091243
Jamesnl55555 <jamesnl55555@gmail.com>
2025-08-26 22:54:24 +0800

    Activity 10: Add React About Me project

---
+++ 10_getting-started-react/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 id="heading">About Me</h1>
+        <hr></hr>
+        <p> <span id="bold">Name: </span> Jimmy Lumaad</p>
+        <p> <span id="bold">Course/Year: </span> BS Information Technology 3rd Year</p>
+        <p> <span id="bold">Fun Fact: </span> I dont like to eat fish</p>
+        <h1 id="heading">Why I Want to Learn Git & GitHub</h1>
+        <hr></hr>
+        <p>Github is a place where I can collaborate with my peers, it is essential in making projects as I could store my projects there too. For me to use it, I need to learn how to use it, that is why I want to learn Git and Github</p>
+      </div>
+    </>
+  )
+}
+
+export default App
