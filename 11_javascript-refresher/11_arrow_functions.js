12ea35f83b7ea11c7eb15acfc3fa186b69ebefad
Jamesnl55555 <jamesnl55555@gmail.com>
2025-09-06 02:54:42 +0800

    Add arrow functions activity

---
+++ 11_javascript-refresher/11_arrow_functions.js
@@ -0,0 +1,13 @@
+const greet = name => "Hello, " + name;
+
+const square = (n) => {
+  return n * n;
+}
+
+const sayHi = () =>{
+  console.log("Hi!");
+}
+
+console.log(greet("Jimmy"));
+console.log("Square of 5 is " + square(5));
+sayHi();
