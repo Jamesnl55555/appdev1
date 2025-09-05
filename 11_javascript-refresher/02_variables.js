588eb1d7a2907243cb8e16d0e34cc34f6616e38c
Jamesnl55555 <jamesnl55555@gmail.com>
2025-09-06 02:45:42 +0800

    Add variables activity

---
+++ 11_javascript-refresher/02_variables.js
@@ -0,0 +1,40 @@
+let name = "Jimmy";
+let age = 20;
+let isStudent = true;
+console.log(
+    name,
+    age,
+    isStudent);
+
+typeof name;
+typeof age;
+typeof isStudent;
+console.log(
+    typeof name,
+    typeof age,
+    typeof isStudent);
+
+let num1 = 5;
+let num2 = 5;
+let Add = num1 + num2;
+let Subtract = num1 - num2;
+let Multiply = num1 * num2;
+let Divide = num1 / num2;
+console.log("Add: " + Add);
+console.log("Subtract: " + Subtract);
+console.log("Multiply: " + Multiply);
+console.log("Divide: " + Divide);
+
+if("5" === 5){
+    console.log(" === True");
+}
+else{
+    console.log(" === False");
+}
+
+if("5" == 5){
+    console.log(" == True");
+}
+else{
+    console.log(" == False");
+}
