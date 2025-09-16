9309cf76c662c1391a2ac8023369220799bb1c00
Jamesnl55555 <Jamesnl55555@gmail.com>
2025-09-16 22:07:42 +0800

    Done creating and nesting components

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function Component1(){
+return(
+<><h1>HELLO WORLD</h1></>
+)}
+{/* let num1= "Hello"; */}
 
+function Component2(){
+return(
+<>
+<h1>This is component 2 and nested below is component 1</h1>
+<Component1/>
+</>
+)}
+{/*
+if(true){
+num1="Hi";
+}
+else{
+num1="Low";
+}
+*/}
+function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <Component1/>
+     <h1>=====================</h1>
+     <h1>Below is component 2</h1>
+     <Component2/>
+     {/*<h1>This is a data of num1 variable: {num1}</h1>*/}
     </>
   )
 }
