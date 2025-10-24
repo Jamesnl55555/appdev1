95589d26c1a4fd44cfc4a0862886527a5d3c8f40
Jamesnl55555 <jamesnl55555@gmail.com>
2025-10-24 15:58:54 +0800

    Done with how to add markup to JavaScript with JSX

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -4,7 +4,19 @@ function App() {
 
   return (
     <>
-      <Gallery />
+      <h1>Hedy Lamarr's Todos</h1>
+      <img
+        src="https://i.imgur.com/yXOvdOSs.jpg"
+        alt="Hedy Lamarr"
+        className="photo"
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve spectrum technology</li>
+      </ul>
+    
+
     </>
   )
 }
