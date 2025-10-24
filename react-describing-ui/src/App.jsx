48916625025f8ab80ce3e81d210d7de7817a59b0
Jamesnl55555 <jamesnl55555@gmail.com>
2025-10-24 16:44:22 +0800

    Done with how to render multiple components at a time

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,31 +1,25 @@
-function Item({ name, isPacked }) {
-  return (
-    <li className="item">
-      {name} {isPacked && '✅'}
-    </li>
-  );
-}
-
+import { people } from "./utilities/Data";
+import { Utils } from "./utilities/Utils";
 
 function App() {
+   const listItems = people.map(person =>
+    <li key={person.id}>
+      <img
+        src={Utils(person)}
+        alt={person.name}
+      />
+      <p>
+        <b>{person.name}:</b>
+        {' ' + person.profession + ' '}
+        known for {person.accomplishment}
+      </p>
+    </li>
+  );
   return (
-    <section>
-      <h1>Sally Ride's Packing List</h1>
-      <ul>
-        <Item
-          isPacked={true}
-          name="Space suit"
-        />
-        <Item
-          isPacked={true}
-          name="Helmet with a golden leaf"
-        />
-        <Item
-          isPacked={false}
-          name="Photo of Tam"
-        />
-      </ul>
-    </section>
+    <article>
+      <h1>Scientists</h1>
+      <ul>{listItems}</ul>
+    </article>
   );
 
 }
