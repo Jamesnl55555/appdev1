154f0ab90e0eb58bdc1b0085d18bc3397191e514
Jamesnl55555 <jamesnl55555@gmail.com>
2025-10-24 16:37:20 +0800

    Done with how to conditionally render components

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,11 +1,33 @@
-import Profile from "./components/Profile"
-function App() {
-
+function Item({ name, isPacked }) {
   return (
-    <>
-     <Profile />
-    </>
-  )
+    <li className="item">
+      {name} {isPacked && '✅'}
+    </li>
+  );
+}
+
+
+function App() {
+  return (
+    <section>
+      <h1>Sally Ride's Packing List</h1>
+      <ul>
+        <Item
+          isPacked={true}
+          name="Space suit"
+        />
+        <Item
+          isPacked={true}
+          name="Helmet with a golden leaf"
+        />
+        <Item
+          isPacked={false}
+          name="Photo of Tam"
+        />
+      </ul>
+    </section>
+  );
+
 }
 
 export default App
