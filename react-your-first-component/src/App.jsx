a9f8ec331182dfe862f98fe0717ee49899c9a42a
Jamesnl55555 <jamesnl55555@gmail.com>
2025-11-09 11:22:26 +0800

    Done with defining a component

---
+++ react-your-first-component/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div>
+      <img
+      src="https://i.imgur.com/MK3eW3Am.jpg"
+      alt="Katherine Johnson"
+    />
+    </div>
+    
+  )
+}
