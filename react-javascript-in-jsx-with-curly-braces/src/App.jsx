06e14648ba9f75fd6a4be0bb8c97aafc17385b9e
Jamesnl55555 <jamesnl55555@gmail.com>
2025-11-23 19:00:09 +0800

    Done with using curly braces: A window into the JavaScript world

--- react-javascript-in-jsx-with-curly-braces/src/App.jsx
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -1,8 +1,14 @@
-import Avatar from "./Avatar";
+// import Avatar from "./Avatar";
+import TodoList from "./TodoList";
+
 
 export default function App() {
  return(
   // Passing strings with quotes
-  <Avatar />
+  // <Avatar />
+  // =====================
+  // Using curly braces: A window into the JavaScript world
+  <TodoList />
+
  );
 }
